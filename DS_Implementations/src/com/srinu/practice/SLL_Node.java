77728cf5172ab853e38ad9004c2f39164c27577b
stgk4 <stgk4@mst.edu>
2017-08-28 16:48:21 -0500

    implemented single linkedlist node

--- DS_Implementations/src/com/srinu/practice/SLL_Node.java
+++ DS_Implementations/src/com/srinu/practice/SLL_Node.java
@@ -1,5 +1,21 @@
 package com.srinu.practice;
 
 public class SLL_Node {
-
+	SLL_Node next;
+	int data;
+	
+	public SLL_Node(int data) {
+		this.data = data;
+		next = null;
+	}
+	
+	public void appendToTail(int d) {
+		SLL_Node new_node = new SLL_Node(d);
+		SLL_Node n = this;
+		
+		while(n.next!=null) {
+			n=n.next;
+		}
+		n.next = new_node;
+	}
 }
