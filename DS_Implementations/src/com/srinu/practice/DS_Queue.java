c20872e98f382b3bd1bdec9b4e157ae4dba846a2
stgk4 <stgk4@mst.edu>
2017-08-28 18:08:08 -0500

    added implementation for queue ds

--- DS_Implementations/src/com/srinu/practice/DS_Queue.java
+++ DS_Implementations/src/com/srinu/practice/DS_Queue.java
@@ -1,5 +1,23 @@
 package com.srinu.practice;
 
 public class DS_Queue {
-
+	Node_SLL first, last;
+	
+	public void enqueue(int d) {
+		Node_SLL new_node = new Node_SLL(d);
+		last.next = new_node;
+		last = new_node;
+		if(first==null) {
+			first = last;
+		}
+	}
+	
+	public Node_SLL dequeue(int d) {
+		if(first==null) {
+			return null;
+		}
+		Node_SLL removed_node = first;
+		first=first.next;
+		return removed_node;
+	}
 }
