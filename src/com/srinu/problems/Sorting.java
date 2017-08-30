379d7efb268b1775ee34706687420d807b811184
stgk4 <stgk4@mst.edu>
2017-08-29 23:53:35 -0500

    added a package for problems

---
+++ src/com/srinu/problems/Sorting.java
@@ -0,0 +1,23 @@
+package com.srinu.problems;
+
+public class Sorting {
+	
+	public static void main(int[] args) {
+		
+	}
+	
+	/*
+	 * Question: Given two sorted arrays A and B, A has large enough buffer at the end to 
+	 * hold B. Write a method to merge A and B
+	 * 
+	 * Solution: Will use merge sort
+	 * to merge the two arrays, this will take linear time
+	 * 
+	 * pseudo code: 
+	 * 1create pointers A-pointer=A.size-1, B-pointer=B.size-1
+	 * 2) compare A[left] and B[right] and the highest will go into A[right] and decrement the lowest side index 
+	 * 4) if the left is less than A.size-1
+	 * 
+	 */
+
+}
