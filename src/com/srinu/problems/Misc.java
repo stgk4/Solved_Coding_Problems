93caa67a4683e87f52de7754cc3e076ef6defa5e
stgk4 <stgk4@mst.edu>
2017-08-31 23:12:13 -0500

    some more tree problems

--- src/com/srinu/problems/Misc.java
+++ src/com/srinu/problems/Misc.java
@@ -6,10 +6,50 @@ public class Misc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = {1,3, 5, 6, 8, 2, 9};
-		System.out.println(findPair(a,10));
+		//int[] a = {1,3, 5, 6, 8, 2, 9};
+		//System.out.println(findPair(a,10));
+		
+		Node n= new Node(1);
+		n.next = new Node(2);
+		n.next.next = new Node(3);
+		
+		//Node n_r = reverseLinkedList(n);
+		
+		printLinkedList(reverseLinkedList_rec(n,null));
 	}
 	
+	public static Node reverseLinkedList(Node n) {
+		if(n==null || n.next==null) return n;
+		
+		Node prev = null;
+		while(n!=null) {
+			Node next = n.next;
+			n.next = prev;
+			prev = n;
+			n=next;
+		}
+		return prev;
+	}
+	
+	public static Node reverseLinkedList_rec(Node current, Node prev) {
+		if(current==null) return prev;
+		Node next = current.next;
+		current.next = prev;
+		return reverseLinkedList_rec(next, current);
+	}
+	
+	public static void printLinkedList(Node n) {
+		//if(n==null) return;
+		while (n!=null) {
+			System.out.print(n.data + "->");
+			n=n.next;
+		}
+		System.out.print("NULL");
+	}
+	
+	/*
+	 * return true if an array has pair of numbers whose sum is equal to value
+	 */
 	public static boolean findPair(int[] a, int val) {
 		Set<Integer> foundValues = new HashSet<Integer>();
 		for(int i: a) {
@@ -20,5 +60,27 @@ public class Misc {
 		}
 		return false;
 	}
+	
+	public static boolean findPair2(int[] a, int val) {
+		int left = 0;
+		int right=a.length-1;
+		int sum;
+		for(;left<right;){
+			sum = a[left]+a[right];
+			if(sum==val) return true;
+			else if(sum>val) --right;
+			else ++left;
+		}
+		return false;
+	}
 
+}
+
+class Node{
+	Node next;
+	int data;
+	Node(int data){
+		this.data = data;
+		next = null;
+	}
 }
--- src/com/srinu/problems/TreesGraphs.java
+++ src/com/srinu/problems/TreesGraphs.java
@@ -4,23 +4,56 @@ public class TreesGraphs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Nodee root = new Nodee(1);
+		root.left= new Nodee(2);
+		root.right= new Nodee(3);
+		root.left.left= new Nodee(4);
+		root.left.right= new Nodee(5);
+		root.right.left = new Nodee(6);
+		root.right.right= new Nodee(7);
+		System.out.println("HeightOfTree:"+getHeight(root));
+		printLevelOrder(root);
 	}
 	
 	/*
 	 * Method to find the height of the tree
 	 */
-	public static int getHeight(TreeNode root) {
-		if(root==null) return 0; //base case
+	public static int getHeight(Nodee root) {
+		if(root==null) return 0;
 		return Math.max(getHeight(root.left), getHeight(root.right))+1;
+	}
+	/*
+	 * print elements in the level order of tree
+	 */
+	public static void printLevelOrder(Nodee root) {
+		if(root==null) return;
+		int height = getHeight(root);
+		for(int i=1; i<=height; i++) {
+			System.out.println("Level-"+i);
+			printAtLevel(root,i);
+		}
+	}
+	
+	/*
+	 * prints elements at a given level
+	 */
+	public static void printAtLevel(Nodee root, int h) {
+		if(root==null || h<1) return;
+		
+		if(h==1) {
+			System.out.println(root.data);
+		}else {
+			printAtLevel(root.left, h-1);
+			printAtLevel(root.right, h-1);
+		}
 	}
 	
 	/*
 	 * Method to check if a binary tree is balanced. 
 	 * Note: A balanced is defined to be a tree such that the heights of the two 
-	 * subtrees of any node never differ by more than one
+	 * subtrees of any Nodee never differ by more than one
 	 */
-	public static boolean isBalanced(TreeNode root) {
+	public static boolean isBalanced(Nodee root) {
 		if (root==null) return true; //base case
 		
 		int heightDiff = getHeight(root.left)-getHeight(root.right);
@@ -33,13 +66,14 @@ public class TreesGraphs {
 	}
 	
 	
-	
-	
 
 }
 
-class TreeNode{
-	TreeNode left, right;
+class Nodee{
+	Nodee left, right;
 	int data;
+	Nodee(int data){
+		this.data = data;
+	}
 	
 }
