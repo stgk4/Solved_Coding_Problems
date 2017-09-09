38eb2ef3051eb9b29a4780cc385bf9d6ce115b0a
stgk4 <stgk4@mst.edu>
2017-09-08 22:58:00 -0500

    method description for graph creation from array with slash and backslash problem

--- src/com/srinu/problems/Create_Graph.java
+++ src/com/srinu/problems/Create_Graph.java
@@ -1,10 +1,26 @@
 package com.srinu.problems;
+import java.util.Hashtable;
+import java.util.ArrayList;
 
 public class Create_Graph {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static Hashtable<Integer, Node> createGraph() {
+		
+		return null;
+	}
+	
+	class Node{
+		int node_num;
+		ArrayList<Node> adjacent;
+		
+		 Node(int node_num){
+			 this.node_num = node_num;
+		 }
 	}
 
 }
