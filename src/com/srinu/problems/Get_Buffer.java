5e8bc35913a475ad30ad0d280e4716adf0bb8195
stgk4 <stgk4@mst.edu>
2017-09-08 22:53:50 -0500

    method description for 4k buffer problem

--- src/com/srinu/problems/Get_Buffer.java
+++ src/com/srinu/problems/Get_Buffer.java
@@ -4,7 +4,17 @@ public class Get_Buffer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		char [] buffer = new char[8*1024];
+		System.out.println(read_data(buffer));
+	}
+	
+	public static int read_data(char[] buffer) {
+		
+		return -1;
+	}
+	
+	public static int get4K_data(char[] buffer) {
+		return -1;
 	}
 
 }
