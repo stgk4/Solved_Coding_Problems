fe2eed1a94b557c46d8af6f2994f7a30a6e236e7
stgk4 <stgk4@mst.edu>
2017-09-12 12:40:28 -0500

    method to remove pair and target sum in an array

--- src/com/srinu/problems/FindPairs.java
+++ src/com/srinu/problems/FindPairs.java
@@ -1,27 +1,45 @@
 package com.srinu.problems;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 public class FindPairs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(1,3,4,5,6,8,9));
+		ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(48,20,1,3,4,6,9,24));
+		Collections.sort(array);
+		int target_index=0;
+		while(array.size()>0 && target_index<array.size()) {
+			Pair output = getTwoPair(array,target_index);
+			System.out.println(target_index + ":" + (output==null? "null":output.left+","+output.right));
+			if(output!=null) {
+				array.remove(target_index);
+				array.remove(output.right);
+				array.remove(output.left);
+				target_index=0;
+			}else {
+				target_index++;
+			}
+			
+		}
 		
-		Pair output = getTwoPair(array,9);
-		System.out.println(output.left + "," + output.right);
+		for(int i: array) {
+			System.out.println(i);
+		}
+		
 
 	}
 	
-	public static Pair getTwoPair(ArrayList<Integer> array, int num) {
+	public static Pair getTwoPair(ArrayList<Integer> array, int target_index) {
 		int low =0;
 		int high = array.size()-1;
+		int num = array.get(target_index);
 		
 		while(low<=high) {
 			int sum = array.get(low)+array.get(high);
-			if(sum==num) {
+			if(sum==num && target_index!=low && target_index!=high && low!=high) {
 				return new Pair(low,high);
 			}
 			if(sum<num) {
