9bcef9abda7c8ce6b4585dcce8522e572beaf59c
stgk4 <stgk4@mst.edu>
2017-09-20 23:49:42 -0500

    test

--- src/com/srinu/problems/RandomNumGen.java
+++ src/com/srinu/problems/RandomNumGen.java
@@ -3,7 +3,7 @@ package com.srinu.problems;
 public class RandomNumGen{
 
     public static void main(String args[]){
-        
+        System.out.println(generateRandomNum());
     }
     
     public static int generateRandomNum(){
