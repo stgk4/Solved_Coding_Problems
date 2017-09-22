39867209e05807586f4693bd3b488b468aba8ab0
stgk4 <stgk4@mst.edu>
2017-09-21 23:28:49 -0500

    random number function;

--- src/com/srinu/problems/RandomNumGen.java
+++ src/com/srinu/problems/RandomNumGen.java
@@ -1,23 +1,27 @@
 package com.srinu.problems;
 
 public class RandomNumGen{
-
+	
+	public static int previous = 0;
+	public static int a = 252; //25214903917
+	public static int c=11;
     public static void main(String args[]){
-        System.out.println(generateRandomNum());
+    	for(int i=0; i<100; i++){
+    		System.out.println(generateRandomNum());
+    	}
     }
     
+    
     public static int generateRandomNum(){
-    	int num=3;
-    	for(int i=0; i<=10; i++){
-    		 
-    	}
-    	return num;
+    	int r = a*previous + c;
+    	previous = r;
+    	return r;
     }
     
     public static int generateRandomNum(int min, int max){
     	int a = generateRandomNum();
     	int b = generateRandomNum();
-    	int num = max+min - (a+b);
+    	int num = min+ Math.abs((a%min)+(b%max));
     	return num;
     }
 
