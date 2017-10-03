ed48b37e9197c6cc5678d1ed04e391551cf96ead
stgk4 <stgk4@mst.edu>
2017-10-03 17:36:32 -0500

    maximum sum increasing sequence

--- src/com/srinu/subarrays/MaxSumIncSequence.java
+++ src/com/srinu/subarrays/MaxSumIncSequence.java
@@ -7,9 +7,28 @@ public class MaxSumIncSequence {
 		System.out.println(getMaxSumIS(array, n));
 	}
 	
+	/*
+	 * get maximum sum increaseing subsequence
+	 */
 	public static int getMaxSumIS(int[] array, int n){
-		int result =0;
-		return result;
+		int max = array[0];
+		int i, j;
+		int msis[] = new int[n];
+		
+		//initialize msis values for all indexes same as the array vals
+		for(i=0; i<n; i++) msis[i] = array[i];
+		
+		//compute maximum sum values in bottom up manner
+		for(i=1; i<n; i++){
+			for (j=0; j<i; j++){
+				if(array[i]>array[j] && msis[i]<msis[j]+array[i]){
+					msis[i] = msis[j]+array[i];
+				}
+			}		
+			if (max<msis[i]) max = msis[i];
+		}		
+		
+		return max;
 	}
 
 }
