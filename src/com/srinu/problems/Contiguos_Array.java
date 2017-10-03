b8eff7ba6f1a0436bb56dd9f23d0276dbd898291
stgk4 <stgk4@mst.edu>
2017-10-02 20:43:39 -0500

    added method to return subarray indices whose sum is equal to given sum

--- src/com/srinu/problems/Contiguos_Array.java
+++ src/com/srinu/problems/Contiguos_Array.java
@@ -4,7 +4,29 @@ public class Contiguos_Array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int [] array = {1,12, 4, 3, 2, 7, 3};
+		subArrayEqualsSum(array, 9);
+	}
+	
+	public static void subArrayEqualsSum(int[] array, int sum){
+		int cur_sum = array[0];
+		int start = 0;
+		int size = array.length;
+		for(int i=1; i<=size; i++){
+			while(cur_sum>sum && start<i-1){
+				cur_sum-=array[start];
+				start++;
+			}
+			if(cur_sum==sum){
+				System.out.println(start + "," + (i-1));
+				return;
+			}
+			if(i<size){
+				cur_sum+=array[i];
+			}
+		}
+		System.out.println(-1);
+		return;
 	}
 
 }
