6028a969ac9e8223e795d9c415b85ef00ab69e6d
stgk4 <stgk4@mst.edu>
2017-10-02 21:24:53 -0500

    getting equilibrium index of an array such that sum of elements to its left is equal to the sum of elements to the right

--- src/com/srinu/problems/Contiguos_Array.java
+++ src/com/srinu/problems/Contiguos_Array.java
@@ -4,12 +4,27 @@ public class Contiguos_Array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] array = {1,12, 4, 3, 2, 7, 3};
-		subArrayEqualsSum(array, 9);
+		int [] array = {1,12, 4, 3, 2, 7, 1};
+		//subArrayEqualsSum(array, 9);
+		System.out.println(getEquilibriumPoint(array));
 	}
 	
 	public static int getEquilibriumPoint(int[] array){
+		int size = array.length;
+		if(size==1) return 0;
 		
+		int sum_array[] = new int[size];
+		sum_array[0] = array[0];
+		for(int i=1;i<size;i++){
+			sum_array[i]=sum_array[i-1]+array[i];
+		}
+		int array_sum=sum_array[size-1];
+		
+		for(int i=1;i<size-1;i++){
+			if(sum_array[i-1]==(array_sum-sum_array[i-1]-array[i]))
+				return i;
+		}
+		return -1;
 	}
 	
 	public static void subArrayEqualsSum(int[] array, int sum){
