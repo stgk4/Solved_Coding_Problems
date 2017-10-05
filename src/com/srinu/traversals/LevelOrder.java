11c3f4e9cc999b7434a04f06a5253251bfa796c8
stgk4 <stgk4@mst.edu>
2017-10-05 12:28:58 -0500

    connect same level using null markers

--- src/com/srinu/traversals/LevelOrder.java
+++ src/com/srinu/traversals/LevelOrder.java
@@ -14,9 +14,36 @@ public class LevelOrder {
 		node.left.right = new Node(1);
 		node.right.left = new Node(6);
 		node.right.right = new Node(8);
-		printLevelOrder(node);
+		connectSameLevel_usingLevel(node);
 	}
 	
+	
+	
+	private static void connectSameLevel_usingLevel(Node root) {
+		// TODO Auto-generated method stub
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(root);
+		int cur_level = 1;
+		root.level = cur_level;
+		while(!queue.isEmpty()){
+			Node n = queue.remove();
+			if(!queue.isEmpty() && n.level!=cur_level){
+				if(n.left!=null) 
+				{
+					n.left.level = cur_level;
+					queue.add(n.left);
+				}
+			}
+			else{
+				cur_level+=1;
+			}
+		}
+		
+		
+	}
+
+
+
 	/*
 	 * Printing level order using breadth first search
 	 * This runs in O(n) time and takes O(n) space for recursion stack
@@ -51,19 +78,45 @@ public class LevelOrder {
 	}
 	
 	/*
+	 * Connect same level nodes (i.e. next right)
+	 * This is an improvement of BFS/LevelOrder using Queue
+	 * This is implemented by adding null markers after
+	 * end of each level
+	 */
+	
+	public static void connectSameLevel(Node root){
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(root);
+		queue.add(null); //end of level-1
+		
+		while(!queue.isEmpty()){
+			Node n = queue.remove();
+			if(n!=null){
+				n.nextRight = queue.peek();
+				System.out.println(n.data+"->" + ((n.nextRight!=null) ? n.nextRight.data:"null"));
+				if(n.left!=null) queue.add(n.left);
+				if(n.right!=null) queue.add(n.right);
+			}else{
+				if(!queue.isEmpty()) queue.add(null);
+			}
+		}
+	}
+	
+	
+	/*
 	 * Prints the binary tree in a spiral pattern:
 	 * meaning, the even level number rows will print
 	 * from left to right, and odd number level will
-	 * print from right to left
+	 * print from right to left (Note:root level is 1)
 	 */
 	public static void printSpiralLevelOrder(Node root){
 		Stack<Node> s1 = new Stack<Node>();
 		Stack<Node> s2 = new Stack<Node>();
 		s1.push(root);
 		while(!s1.isEmpty() || !s2.isEmpty()){
-			/*This while loop visits/prints from left to right 
-			 * and adds numbers to other stack(s2) in reverse order
-			 * so that other while loop prints in reverse order
+			/*This while loop visits/prints from right to left 
+			 * and adds children to other stack(s2) in reverse order
+			 * so that other while loop prints in correct order
 			 */
 			while(!s1.isEmpty()){
 				Node n = s1.pop();
@@ -73,8 +126,9 @@ public class LevelOrder {
 			}
 			
 			/*
-			 * This visits in reverse order and 
-			 * adds in right order to the Stack(s1)
+			 * This visits in correct order and 
+			 * adds children in correct order to the Stack(s1)
+			 * so that it prints in reverse order
 			 */
 			while(!s2.isEmpty()){
 				Node n = s2.pop();
@@ -97,6 +151,7 @@ public class LevelOrder {
 class Node{
 	int data;
 	Node left, right, nextRight;
+	int level;
 	Node(int data){
 		this.data=data;
 		left = null;
