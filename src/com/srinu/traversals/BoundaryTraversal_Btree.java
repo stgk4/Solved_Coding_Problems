deb3b558c5ef245d29cf3cd4e0dbeaaf5671b5d9
stgk4 <stgk4@mst.edu>
2017-10-08 20:55:19 -0500

    Boundary traversal: left, leaves, right

--- src/com/srinu/traversals/BoundaryTraversal_Btree.java
+++ src/com/srinu/traversals/BoundaryTraversal_Btree.java
@@ -1,5 +1,7 @@
 package com.srinu.traversals;
-
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 public class BoundaryTraversal_Btree {
 
 	public static void main(String[] args) {
@@ -18,7 +20,53 @@ public class BoundaryTraversal_Btree {
 	}
 	
 	public static void printBoundary(Node node){
+		printLeftBoundary(node);
+		printLeafNodes(node);
+		printRightBoundary(node);
+	}
+	
+	public static void printLeftBoundary(Node node){
+		if(node!=null){//ensure valid node
+			if(node.left!=null){//ensure if left node exists
+				System.out.print(node.data + " ");
+				printLeftBoundary(node.left);
+			}
+			else if(node.right!=null){//ensure if at least right node exists
+				System.out.print(node.data + " ");
+				printLeftBoundary(node.right);
+			}
+			else{
+				//this is the case where both left and right nodes are null
+				//and we ignore this case to avoid duplicates
+			}
+		}
+	}
+	
+	public static void printLeafNodes(Node node){
+		if(node==null) return;
 		
+		printLeafNodes(node.left);
+		
+		if(node.left==null && node.right == null)
+			System.out.print(node.data + " ");
+		
+		printLeafNodes(node.right);
+	}
+	
+	public static void printRightBoundary(Node node){
+		printRightBoundary(node, node);
+	}
+	
+	public static void printRightBoundary(Node node, Node root){
+		if(node!=null){
+			if(node.right!=null){
+				printRightBoundary(node.right, root);
+				if(node!=root) System.out.print(node.data + " ");
+			}else if(node.left!=null){
+				printRightBoundary(node.left, root);
+				System.out.print(node.data + " ");
+			}
+		}
 	}
 
 }
