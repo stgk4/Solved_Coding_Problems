cda9476a84fd3c686f95556581bc7985eca7de44
stgk4 <stgk4@mst.edu>
2017-10-11 12:03:13 -0500

    graph implementation

--- src/com/srinu/graphs/Graph.java
+++ src/com/srinu/graphs/Graph.java
@@ -7,6 +7,7 @@ public class Graph {
 	 */
 	class Edge implements Comparable<Edge>{
 		int src, dest, weight;
+		
 		public int compareTo(Edge compareEdge){
 			return this.weight -compareEdge.weight;
 		}
