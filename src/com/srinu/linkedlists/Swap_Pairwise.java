06a4a24eb952374798a2bb14edaffe8d220ae26b
stgk4 <stgk4@mst.edu>
2017-10-10 22:59:44 -0500

    graph traversal algos

--- src/com/srinu/linkedlists/Swap_Pairwise.java
+++ src/com/srinu/linkedlists/Swap_Pairwise.java
@@ -4,7 +4,52 @@ public class Swap_Pairwise {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = new Node(4);
+		head.next.next.next.next = new Node(5);
+		printLinkedList(head);
+		head = pairWiseSwap(head);
+		printLinkedList(head);
+		
 	}
+	
+	public static Node pairWiseSwap(Node head){
+		Node prev=null, right = null;
+		Node left = head;
+		if(left.next!=null) head = left.next;
+		while(left!=null){
+			right = left.next;
+			
+			if(right==null) return head;
+			left.next = right.next;
+			right.next = left;
+			if(prev!=null) prev.next = left;
+			prev = left;
+			
+			left = left.next;
+		}
+		return head;  
+	}
+	
+	public static void printLinkedList(Node head){
+		
+		while(head.next!=null){
+			System.out.print(head.data + "->");
+			head = head.next;
+		}
+		System.out.print(head.data + "->Null\n");
+	}
+	
 
+}
+
+class Node{
+	int data;
+	Node next;
+	Node(int data){
+		this.data = data;
+		next = null;
+	}
 }
