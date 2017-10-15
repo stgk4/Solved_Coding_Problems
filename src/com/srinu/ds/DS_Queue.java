12690178af92158507e27122f985ddb5da2c36e2
stgk4 <stgk4@mst.edu>
2017-10-15 14:22:27 -0500

    added some comments

--- src/com/srinu/ds/DS_Queue.java
+++ src/com/srinu/ds/DS_Queue.java
@@ -5,19 +5,30 @@ public class DS_Queue {
 	
 	public void enqueue(int d) {
 		Node_SLL new_node = new Node_SLL(d);
-		last.next = new_node;
-		last = new_node;
-		if(first==null) {
+		//case when the queue is empty
+		if(last==null){
+			last = new_node;
 			first = last;
+		}
+		//case when the queue is non-empty
+		else{
+			last.next = new_node;
+			last = new_node;
 		}
 	}
 	
 	public Node_SLL dequeue(int d) {
-		if(first==null) {
+		if(first==null) { //case when the node is empty
 			return null;
 		}
+	
+		//getting hold of the removed node
 		Node_SLL removed_node = first;
+		
 		first=first.next;
+		
+		if(first==null) last = first;//if the queue become empty after dequeueing the only element that was present
+		
 		return removed_node;
 	}
 }
