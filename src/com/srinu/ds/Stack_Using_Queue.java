192bf96a4e2808ae6ca98d4be7777ae3a2049601
stgk4 <stgk4@mst.edu>
2017-10-15 15:03:25 -0500

    comments and swap method

--- src/com/srinu/ds/Stack_Using_Queue.java
+++ src/com/srinu/ds/Stack_Using_Queue.java
@@ -8,9 +8,11 @@ public class Stack_Using_Queue {
 	Queue<Integer> queue2 = new ArrayDeque<Integer>();
 	
 	public boolean isEmpty(){
-		return (queue1.size()+queue2.size() == 0);
+		return (queue1.size()+queue2.size() == 0); //ideally the size is always the size of queue, but just in case of synchronous queue
 	}
 	
+	//This queue implementation prioritizes the the pop(), i.e O(1) popping time 
+	// and O(N) pushing time; alternatively the other way out can also be implemented
 	public int pop() throws Exception{
 		if(isEmpty()){
 			throw new Exception("Stack is empty");
@@ -18,6 +20,8 @@ public class Stack_Using_Queue {
 		return queue1.remove();
 	}
 	
+	//we add all the old elements of queue1 to queue2 only after
+	// adding new element into queue2 and then swap the queues
 	public void push(int a){
 		if(queue1.isEmpty()){
 			queue1.add(a);
@@ -26,8 +30,16 @@ public class Stack_Using_Queue {
 			while(!queue1.isEmpty()){
 				queue2.add(queue1.remove());
 			}
-			queue1 = queue2;
-			queue2.clear();
+			swapQueues(queue1,queue2);
 		}
+	}
+	
+	//method to swap queues using references, rather than adding element by element or copying  and clearing to
+	//save a linear pass
+	public void swapQueues(Queue<Integer> q1, Queue<Integer> q2){
+		Queue<Integer> temp = q1;
+		q1 = q2;
+		q2 = temp;
+		
 	}
 }
