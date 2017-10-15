1c15e180927a0e2db6c56856ba58a440ce6e24ef
stgk4 <stgk4@mst.edu>
2017-10-15 15:14:51 -0500

    comments for counting islands in a 2d matrix

--- src/com/srinu/graphs/CountIslands.java
+++ src/com/srinu/graphs/CountIslands.java
@@ -2,13 +2,17 @@ package com.srinu.graphs;
 
 public class CountIslands {
 	
+	//This is for deciding next neighbor from the current (i,j) position
 	public int[] row_inc =  {-1, -1, -1,  0, 0,  1, 1, 1};
 	public int[] col_inc = {-1,  0,  1, -1, 1, -1, 0, 1};
 	
+	//traversing element by element in the matrix
 	public int countIslands(int[][] M) {
 		int ROW = M.length;
 		int COL = M[0].length;
 		
+		//a boolean array analogous to the given matrix for keeping track
+		//of the visited elements
 		boolean visited[][] = new boolean[ROW][COL];
 		
 		int count =0;
@@ -23,14 +27,16 @@ public class CountIslands {
 		return count;
 	}
 	
+	//DFS Method to mark all the contiguous network forest of one's as visited
 	public void DFS(int[][] M, int i, int j, boolean[][] visited) {
 		visited[i][j] = true;
-		for(int k=0; k<8; k++) 
-			if(canVisit(M, i+row_inc[k], j+col_inc[k], visited))
+		for(int k=0; k<8; k++) //for each neighbor
+			if(canVisit(M, i+row_inc[k], j+col_inc[k], visited)) //visit only if 'canBeVisited'
 				DFS(M, i+row_inc[k], j+col_inc[k], visited);
 	}
 
-	private boolean canVisit(int[][] m, int i, int j, boolean[][] visited) {
+	//check if the node's (i,j) is valid and its value is '1' and not already visited
+	private boolean canVisit(int[][] m, int i, int j, boolean[][] visited){
 		// TODO Auto-generated method stub
 		return (0<=i && i<visited.length && 
 				0<=j && j<visited[0].length &&
@@ -38,9 +44,10 @@ public class CountIslands {
 	}
 
 	public static void main(String[] args) {
-		int M[][] = new int[][]{{1, 1, 0, 0, 0},
+		int M[][] = new int[][]{
+			{1, 1, 0, 0, 0},
             {0, 1, 0, 0, 1},
-            {1, 0, 0, 1, 1},
+            {1, 0, 1, 0, 1},
             {0, 0, 0, 0, 0},
             {1, 0, 1, 0, 1}
            };
