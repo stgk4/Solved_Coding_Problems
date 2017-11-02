6d049a4220fa0604d61c2c6cc3cc5732a1ec7573
stgk4 <stgk4@mst.edu>
2017-11-01 23:16:55 -0500

    added class for belmond ford

---
+++ src/com/srinu/graphs/BelmondFord.java
@@ -0,0 +1,17 @@
+package com.srinu.graphs;
+
+public class BelmondFord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static class GraphNode{
+		int val;
+		public GraphNode(int val){
+			this.val = val;
+		}
+	}
+
+}
