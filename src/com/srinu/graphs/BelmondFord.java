8afe4ea9ede640527854a814463b7da70580e297
stgk4 <stgk4@mst.edu>
2017-11-02 23:32:43 -0500

    add edge method and edge class

--- src/com/srinu/graphs/BelmondFord.java
+++ src/com/srinu/graphs/BelmondFord.java
@@ -12,18 +12,32 @@ public class BelmondFord {
 	public static class Graph{
 		int V;
 		int E;
-		ArrayList<GraphNode> adjacencyList[] = null;
-		public Graph(int v){
+		Edge edges[];
+		public Graph(int v, int e){
 			this.V = v;
-			adjacencyList = new ArrayList[V];
+			this.E = e;
+			edges = new Edge[e];
+			for (int i=0; i<e; i++){
+				edges[i] = new Edge();
+			}
 		}
 	
+		public static void addNode(int val){
+			
+		}
 	}
 	
 	public static class GraphNode{
 		int val;
 		public GraphNode(int val){
 			this.val = val;
+		}
+	}
+	
+	public static class Edge{
+		int src, dest, weight;
+		public Edge(){
+			src = dest = weight =0;
 		}
 	}
 
