bcd20dbfd8e850277015b18d73469be694188712
stgk4 <stgk4@mst.edu>
2017-11-02 23:24:01 -0500

    graph class

--- src/com/srinu/graphs/BelmondFord.java
+++ src/com/srinu/graphs/BelmondFord.java
@@ -1,10 +1,23 @@
 package com.srinu.graphs;
 
+import java.util.ArrayList;
+
 public class BelmondFord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static class Graph{
+		int V;
+		int E;
+		ArrayList<GraphNode> adjacencyList[] = null;
+		public Graph(int v){
+			this.V = v;
+			adjacencyList = new ArrayList[V];
+		}
+	
 	}
 	
 	public static class GraphNode{
