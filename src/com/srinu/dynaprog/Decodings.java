9b9e267e8e41c81db710c8c0a2b7e3795f5a4ec9
stgk4 <stgk4@mst.edu>
2017-11-08 23:51:34 -0600

    added a package for dynamic programming problems

---
+++ src/com/srinu/dynaprog/Decodings.java
@@ -0,0 +1,15 @@
+package com.srinu.dynaprog;
+
+public class Decodings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1 can be decode to A, 2 to B and so on, Given a number '121' can be decoded to ABA, AU, LA
+		
+	}
+	
+	public static int findNumOfDecodings(int[] digits){
+		return -1;
+	}
+
+}
